814044f0c05e69b7d799ab02d16135154f00476a
25093yukinoji <ten.yuki.0601@gmail.com>
2021-02-07 10:52:32 +0000

    [Add]

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,28 +11,28 @@ class User < ApplicationRecord
   #コメント機能
   has_many :book_comments, dependent: :destroy
   #フォロー機能
-  has_many :relationships
-  has_many :followings, through: :relationships, source: :follow
-  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
-  has_many :followers, through: :reverse_of_relationships, source: :user
+  has_many :following_relationships, foreign_key: "follower_id", class_name: "Relationship", dependent: :destroy
+  has_many :followings, through: :following_relationships
+  has_many :follower_relationships, foreign_key: "following_id", class_name: "Relationship", dependent: :destroy
+  has_many :followers, through: :follower_relationships
   #いいね機能
   def already_favorited?(book)
     self.favorites.exists?(book_id: book.id)
   end
   #フォロー機能
+  def following?(other_user)
+    self.followings.include?(other_user)
+  end
+
   def follow(other_user)
     unless self == other_user
-      self.relationships.find_or_create_by(follow_id: other_user.id)
+      self.relationships.find_or_create_by(following_id: other_user.id)
     end
   end
 
   def unfollow(other_user)
-    relationship = self.relationships.find_by(follow_id: other_user.id)
+    relationship = self.relationships.find_by(following_id: other_user.id)
     relationship.destroy if relationship
-  end
-
-  def following?(other_user)
-    self.followings.include?(other_user)
   end
 
   attachment :profile_image, destroy: false
